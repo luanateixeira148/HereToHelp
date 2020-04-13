20fe6c95e0dc97324dfc2bd4c9844570163c6287
= <=>
2020-04-13 12:53:43 -0700

    fixed imgs

--- comps/Header/index.js
+++ comps/Header/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './header.css';
 
+const menuIcon = require('../graphics/menu_icon.svg');
+const smallLogo = require ('../graphics/small_logo.svg');
+
 const Header = () => 
 <div className='header_box'>
     <div className='menu_icon_box'>
-        <img class='cat' src='../graphics/menu_icon.svg' />
+        <img id='menu_icon' src={menuIcon} />
     </div>
     <div className='small_logo_box'>
-        <img class='cat' src='../graphics/small_logo.svg' />
+        <img id='small_logo' src={smallLogo} />
     </div>
 </div>;
 
--- comps/Menu/index.js
+++ comps/Menu/index.js
@@ -5,23 +5,23 @@ import { MenuData } from './data';
 const Menu = () => 
 <div className='menu_box'>
     <div className='menuItem'>
-        <img src="" />
+        <img src={MenuData.icon1} />
         <h6>{MenuData.item1}</h6>
     </div>
     <div className='menuItem'>
-        <img src="" />
+        <img src={MenuData.icon2} />
         <h6>{MenuData.item2}</h6>
     </div>
     <div className='menuItem'>
-        <img src="" />
+        <img src={MenuData.icon3} />
         <h6>{MenuData.item3}</h6>
     </div>
     <div className='menuItem'>
-        <img src="" />
+        <img src={MenuData.icon4} />
         <h6>{MenuData.item4}</h6>
     </div>
     <div className='menuItem'>
-        <img src="" />
+        <img src={MenuData.icon5} />
         <h6>{MenuData.item5}</h6>
     </div>
 </div>;
