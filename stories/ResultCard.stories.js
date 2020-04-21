21d153ab95411033a1e1051b2b130abe064da8f0
= <=>
2020-04-21 15:15:15 -0700

    added resultcard

---
+++ stories/ResultCard.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ResultCard from '../comps/ResultCard';
+
+export default {
+    title: 'ResultCard',
+    component: ResultCard,
+};
+
+export const MyResultCard = () => <ResultCard />
