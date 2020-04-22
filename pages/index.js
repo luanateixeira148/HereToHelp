8877a3a60ba8a41d975862c5c933c766f0e71a91
Poone Ashja <you@example.com>
2020-04-22 15:20:30 -0700

    tried the Link thing

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+import Router from "next/router";
+import "./app.css";
+
+
+
+const Index = () =><div>
+    LANDING PAGE
+    <p/>
+  <Link href= "/questionair1"><a> Quiz Card 1</a></Link>
+
+</div>
+
+export default Index;
