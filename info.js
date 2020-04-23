19373bc5334db7c7b3010919760bc1bb8b27594b
에이러네 <eileone@eileone-ui-MacBook-Pro.local>
2020-04-23 23:32:17 +0900

    getting rid of useless filex

--- info.js
+++
@@ -1,8 +0,0 @@
-var info_index = 0;
-info = [
-    {
-        title: "How it works",
-        text1: "<p>Depression is classified as a mood +<br/>+ disorder. It may be described as feelings + <br/> + of sadness, loss, or anger that interfere + <br/> + with a person’s everyday activities.</p>",
-        image: "imgs/depression.png"
-    }
-]
