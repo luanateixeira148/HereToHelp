dbf098daa4434904f433b7e1c4b92672a4db7503
= <=>
2020-04-26 13:30:08 -0700

    created result page

---
+++ pages/ResultPage/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './resultpage.css';
+import Header from '../../comps/Header';
+import Result from '../../comps/Result';
+import ResultCard from '../../comps/ResultCard';
+
+
+
+const ResultPage = ({}) => 
+<div>
+
+
+</div>
+
+ResultPage.defaultPprops = {
+
+}
+
+export default ResultPage;
+
