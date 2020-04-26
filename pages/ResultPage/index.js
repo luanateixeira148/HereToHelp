870da560e70abb2bcb5c549386bd5723202e20a8
= <=>
2020-04-26 14:58:06 -0700

    created result page

--- pages/ResultPage/index.js
+++ pages/ResultPage/index.js
@@ -3,17 +3,19 @@ import './resultpage.css';
 import Header from '../../comps/Header';
 import Result from '../../comps/Result';
 import ResultCard from '../../comps/ResultCard';
-
-
+import CustomButton from '../../comps/custombuttons';
 
 const ResultPage = ({}) => 
 <div>
-
-
+    <Header />
+    <Result />
+    <ResultCard />
+    <div className='resultButton'>
+        <CustomButton text="Resources" />
+    </div>
 </div>
 
 ResultPage.defaultPprops = {
-
 }
 
 export default ResultPage;
