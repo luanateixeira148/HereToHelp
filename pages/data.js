662b42b9d26003ee9eab75f48d6a6aff7f1ff81f
= <=>
2020-05-07 15:14:30 -0700

    working on results

--- pages/data.js
+++ pages/data.js
@@ -6,6 +6,17 @@ export var data = {
     answer5:"",
 }
 
+// sessionStorage.getItem("data").then((val)=>{
+//     ChangeData(JSON.parse(val));
+// })
+
+if(process.browser){
+    var sessiondata = sessionStorage.getItem("data");
+    ChangeData(JSON.parse(sessiondata));
+}
+
+
 export function ChangeData(d) {
     data = d;
+    sessionStorage.setItem("data", JSON.stringify(data));
 }
