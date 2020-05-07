c0e4b4a11f6a64e083b5ce1cbe6e84ab67513bc8
에이러네 <eileone@eileone-ui-MacBook-Pro.local>
2020-05-07 12:39:04 +0900

    Fixed Progression Bar

--- comps/Progression/index.js
+++ comps/Progression/index.js
@@ -6,11 +6,11 @@ const Progression = ({border, backgroundColor, progress}) => <div
 style={{backgroundColor:backgroundColor, border:border}}
 className="bigDiv"
 >
-    <div className="block1" style={{backgroundColor:(progress >= 1) ? "FFF" : "none" }}></div>
-    <div className="block2" style={{backgroundColor:(progress >= 2) ? "FFF" : "none" }}></div>
-    <div className="block3" style={{backgroundColor:(progress >= 3) ? "FFF" : "none" }}></div>
-    <div className="block4" style={{backgroundColor:(progress >= 4) ? "FFF" : "none" }}></div>
-    <div className="block5" style={{backgroundColor:(progress >= 5) ? "FFF" : "none" }}></div>
+    <div className="block1" style={{backgroundColor:(progress >= 1) ? "#FFF" : "none" }}></div>
+    <div className="block2" style={{backgroundColor:(progress >= 2) ? "#FFF" : "none" }}></div>
+    <div className="block3" style={{backgroundColor:(progress >= 3) ? "#FFF" : "none" }}></div>
+    <div className="block4" style={{backgroundColor:(progress >= 4) ? "#FFF" : "none" }}></div>
+    <div className="block5" style={{backgroundColor:(progress >= 5) ? "#FFF" : "none" }}></div>
 </div>;
 
 Progression.defaultProps = {
